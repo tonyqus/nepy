13bf5f0b58681ce650ea73fd98dc7cbe6626cf67
Tony Qu <tonyqus@163.com>
2015-02-28 07:46:25 +0800

    add SqlLiteProvider

---
+++ Nepy.Dictionary.Providers/SqlLite/SqlLiteProvider.cs
@@ -0,0 +1,17 @@
+﻿using Nepy.Dictionary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nepy.Dictionary.Providers
+{
+    public class SqlLiteProvider : IDataProvider
+    {
+
+        public List<IDataNode> Load()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Tests/PhoneParserTest.cs
+++ Tests/PhoneParserTest.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using NUnit.Framework;
 using Nepy.Parsers;
 using Nepy.Core;
-using Nepy.Core;
 
 namespace Nepy.Tests
 {
