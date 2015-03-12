f3788db7dc9d77e831734e2d403918a6528675dc
Tony Qu <tonyqus@163.com>
2015-03-13 07:53:26 +0800

    Nepy Studio Lite first checkin

---
+++ Nepy.Studio/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Nepy.Studio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnParse = new System.Windows.Forms.Button();
+            this.rtbText = new System.Windows.Forms.RichTextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnParse
+            // 
+            this.btnParse.Location = new System.Drawing.Point(610, 54);
+            this.btnParse.Name = "btnParse";
+            this.btnParse.Size = new System.Drawing.Size(75, 23);
+            this.btnParse.TabIndex = 0;
+            this.btnParse.Text = "Parse It";
+            this.btnParse.UseVisualStyleBackColor = true;
+            // 
+            // rtbText
+            // 
+            this.rtbText.Location = new System.Drawing.Point(12, 56);
+            this.rtbText.Name = "rtbText";
+            this.rtbText.Size = new System.Drawing.Size(592, 230);
+            this.rtbText.TabIndex = 1;
+            this.rtbText.Text = "";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Location = new System.Drawing.Point(12, 308);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(673, 260);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Parsed Text";
+            this.columnHeader1.Width = 280;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.DisplayIndex = 2;
+            this.columnHeader2.Text = "Value";
+            this.columnHeader2.Width = 219;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.DisplayIndex = 1;
+            this.columnHeader3.Text = "Result Type";
+            this.columnHeader3.Width = 123;
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(12, 8);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(591, 21);
+            this.tbPath.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(609, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 580);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.rtbText);
+            this.Controls.Add(this.btnParse);
+            this.Name = "Form1";
+            this.Text = "Nepy Studio Lite";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnParse;
+        private System.Windows.Forms.RichTextBox rtbText;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
